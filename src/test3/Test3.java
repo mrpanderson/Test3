69772b95df06c49d9a9e5b1b93a546530d5ecc4c
PAnderson <PAnderson@H-C121-61NLJ.nps.org>
2016-11-15 15:56:25 -0500

    Started project.

--- src/test3/Test3.java
+++ src/test3/Test3.java
@@ -16,6 +16,8 @@ public class Test3 {
      */
     public static void main(String[] args) {
         System.out.println("test3");
+        System.out.println("test3");
+        System.out.println("test3");
     }
     
 }
